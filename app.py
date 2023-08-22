3a377ced2cb7180d8855953ece1c766e9e720af9
Edison <ll.noside.ll@gmail.com>
2023-08-22 23:26:21 +0800

    Design and build index.html template

--- app.py
+++ app.py
@@ -36,10 +36,6 @@ def index():
 
     return rendered_template
 
-@app.route("/section")
-def about():
-    return render_template('section.html')
-
 # Run the Flask app
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
